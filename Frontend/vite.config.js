7674fb9b5ee1c187801a236d8d4efb4294e529e2
Rehan Raja <rehanraja.dev@gmail.com>
2026-06-18 21:11:15 +0530

    update the vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import daisyui from "daisyui";
 
 export default defineConfig({
-  plugins: [tailwindcss(), daisyui],
-  daisyui: {
-    themes: ["forest"],
-  },
+  plugins: [tailwindcss()],
 });
-
